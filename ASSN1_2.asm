ad17aa074190c53a066b072852dd700b26390289
souvik-sarkar04 <148918451+souvik-sarkar04@users.noreply.github.com>
2025-04-26 12:23:35 +0530

    Update_1 ASSN1_2.asm

--- ASSN1_2.asm
+++ ASSN1_2.asm
@@ -8,3 +8,4 @@ ADC A
 MOV H, A
 SHLD 3050H
 HLT
+; WARNING : Do not forget to add H at the end of the memory location number. Also, there should be a preceding 0 for memory locations starting from an alphabet(LDA F100H is wrong, LDA 0F100H is correct)
